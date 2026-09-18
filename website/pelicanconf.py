b4da4b56c0da131133140532d19503f1f2e6df77
M0ebius0ne <miraceti77@gmail.com>
2026-09-18 20:56:23 +0200

    Add modern Flex theme

--- website/pelicanconf.py
+++ website/pelicanconf.py
@@ -8,6 +8,9 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'fr'
 
+# Theme settings
+THEME = 'themes/Flex'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
