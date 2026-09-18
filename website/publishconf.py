9610d79f4424c4cf23a127b7144c7fa7fde88d26
M0ebius0ne <miraceti77@gmail.com>
2026-09-18 21:17:48 +0200

    Fix SITEURL to load CSS correctly

--- website/publishconf.py
+++ website/publishconf.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://ia-radar.github.io'
+SITEURL = 'https://M0ebius0ne.github.io/outils-ia'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
